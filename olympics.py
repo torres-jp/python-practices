1195f23a3c694418a1c5647c4c01e8e591d81d0b
torres-jp <torres.jpw@gmail.com>
2025-06-02 18:59:11 -0300

    Añadir clases para registro de eventos y participantes en el simulador de Juegos Olímpicos

--- olympics.py
+++ olympics.py
@@ -1,15 +1,42 @@
-class Olympics:
+class Players:
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
 
+
+
+class Olympics:
+    
     def __init__(self):
         self.events = []
 
     def event_register(self):
         
-        event = input("Introduce el nombre del evento deportivo")
+        event = input("Introduce el nombre del evento deportivo").strip()
 
-        self.events.append(event)
+        if event in self.events:
+            print(f'El evento {event} ya esta registrado')
+        else:
+            self.events.append(event)
+            print(f'Evento {event} registrado correctamente.')
 
+    def players_register(self):
 
+        if not self.events:
+            print('No hay eventos registrados. Por favor elige uno primero.')
+            return
+
+        name = input('Introduce el nombre de este participante: ').strip()
+        country = input('Introduce el pais del participante').strip()
+        participant = Players(name, country)
+
+        print('Eventos disponibles: ')
+        for index, event in enumerate(self.events):
+            print(f'{event}')
+
+        
+
+olympics = Olympics()
 
 
 print("Simulador de juegos olimpicos")
@@ -26,10 +53,9 @@ while True:
 
 
     option = input('Selecciona una accion: ')
-
     match option:
         case "1":
-            pass
+            olympics.event_register()
         case "2":
             pass
         case "3":
