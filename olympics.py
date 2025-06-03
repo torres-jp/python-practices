94407dacd95fe4f0040f8a582e99ec9ee09ee426
torres-jp <torres.jpw@gmail.com>
2025-06-02 21:19:33 -0300

    Refactor input prompts and improve participant registration logic in Olympics simulator

--- olympics.py
+++ olympics.py
@@ -3,38 +3,53 @@ class Players:
         self.name = name
         self.country = country
 
-
+    
+    def __eq__(self, value:object):
+        pass
 
 class Olympics:
-    
     def __init__(self):
         self.events = []
+        self.participants = {}
 
     def event_register(self):
-        
         event = input("Introduce el nombre del evento deportivo").strip()
 
         if event in self.events:
-            print(f'El evento {event} ya esta registrado')
+            print(f"El evento {event} ya esta registrado")
         else:
             self.events.append(event)
-            print(f'Evento {event} registrado correctamente.')
+            print(f"Evento {event} registrado correctamente.")
 
     def players_register(self):
-
         if not self.events:
-            print('No hay eventos registrados. Por favor elige uno primero.')
+            print("No hay eventos registrados. Por favor elige uno primero.")
             return
 
-        name = input('Introduce el nombre de este participante: ').strip()
-        country = input('Introduce el pais del participante').strip()
+        name = input("Introduce el nombre de este participante: ").strip()
+        country = input("Introduce el pais del participante").strip()
         participant = Players(name, country)
 
-        print('Eventos disponibles: ')
+        print("Eventos disponibles: ")
         for index, event in enumerate(self.events):
-            print(f'{event}')
+            print(f"{index + 1}. {event}")
 
-        
+        event_choice = (
+            int(input("Selecciona el numero del evento para asignar al participante")) - 1
+        )
+
+        if event_choice >= 0 and event_choice < len(self.events):
+            event = self.events[event_choice]
+
+            if participant in self.participants[event]:
+                print(f'El participante {name} de {country} se registro en el evento deportivo {event}')
+            
+            else:
+                self.participants[event].append(participant)
+                print(f'El participante {name} de {country} se registro en el evento deportivo {event}')
+
+
+
 
 olympics = Olympics()
 
@@ -42,7 +57,6 @@ olympics = Olympics()
 print("Simulador de juegos olimpicos")
 
 while True:
-
     print()
 
     print("1. Registro de Eventos")
@@ -51,8 +65,7 @@ while True:
     print("4. Creacion de Informes")
     print("5. Salir del Programa")
 
-
-    option = input('Selecciona una accion: ')
+    option = input("Selecciona una accion: ")
     match option:
         case "1":
             olympics.event_register()
@@ -66,4 +79,4 @@ while True:
             print("Saliendo del simulador ...")
             break
         case _:
-            print('Opcion Incorrecta, selecciona una nueva.')
+            print("Opcion Incorrecta, selecciona una nueva.")
